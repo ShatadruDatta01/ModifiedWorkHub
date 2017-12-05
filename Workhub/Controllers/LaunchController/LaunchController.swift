588ae5290c1a7eaa640ecff422f3ffa128ca8424
ShatadruDatta <datta.shatadru01@gmail.com>
2017-12-06 02:01:04 +0530

    commit

--- Workhub/Controllers/LaunchController/LaunchController.swift
+++ Workhub/Controllers/LaunchController/LaunchController.swift
@@ -16,35 +16,45 @@ class LaunchController: BaseViewController {
     @IBOutlet weak var viewWorkhubConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let concurrentQueue = DispatchQueue(label:DeviceSettings.dispatchQueueName("getToken"), attributes: .concurrent)
-        
-        API_MODELS_METHODS.token(queue: concurrentQueue) { (responseDict,isSuccess) in
-            if isSuccess {
-                AppConstantValues.companyAccessToken = responseDict!["result"]!["data"]["access_token"].stringValue
-                
-            } else {
-            }
-        }
-        
-//        self.imgLogo.alpha = 0.3
-//        NavigationHelper.helper.headerViewController?.isShowNavBar(isShow: false)
-//        Timer.scheduledTimer(timeInterval: 0.3,
-//                             target: self,
-//                             selector: #selector(self.updateView),
-//                             userInfo: nil,
-//                             repeats: false)
-        
+        NavigationHelper.helper.headerViewController?.isShowNavBar(isShow: false)
+        self.tokenAPICall()
         // Do any additional setup after loading the view.
     }
 }
 
 
+// MARK: - Token API Call
+extension LaunchController {
+    func tokenAPICall() {
+        let concurrentQueue = DispatchQueue(label:DeviceSettings.dispatchQueueName("getToken"), attributes: .concurrent)
+        API_MODELS_METHODS.token(queue: concurrentQueue) { (responseDict,isSuccess) in
+            if isSuccess {
+                AppConstantValues.companyAccessToken = responseDict!["result"]!["data"]["access_token"].stringValue
+                self.startingView()
+            } else {
+                
+            }
+        }
+    }
+}
+
+
+// MARK: - Starting View
+extension LaunchController {
+    func startingView() {
+        self.imgLogo.alpha = 0.3
+        NavigationHelper.helper.headerViewController?.isShowNavBar(isShow: false)
+        Timer.scheduledTimer(timeInterval: 0.3,
+                             target: self,
+                             selector: #selector(self.updateView),
+                             userInfo: nil,
+                             repeats: false)
+    }
+}
+
 
 // MARK: - UpdateView
 extension LaunchController {
-    
-    
     /// UpdateView
     func updateView() {
         self.viewWorkhubConstraint.constant = 0
