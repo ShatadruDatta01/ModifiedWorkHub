db3600349572310911e0808de1337893df38dc98
Administrator <administrator@Administrators-Mac-mini.local>
2018-03-19 14:06:20 +0530

    commit

--- Workhub/Controllers/TermsCondsController/TermsCondsController.swift
+++ Workhub/Controllers/TermsCondsController/TermsCondsController.swift
@@ -24,8 +24,6 @@ class TermsCondsController: BaseViewController, UIWebViewDelegate {
         // Do any additional setup after loading the view.
     }
     
-    
-    
     func webViewDidStartLoad(_ webView: UIWebView) {
         circleIndicator.isHidden = false
         circleIndicator.animate()
